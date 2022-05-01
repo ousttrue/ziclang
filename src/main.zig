8f77c2fe587ab899b5f3af0b1e3e2830a63ef17f
ousttrue <ousttrue@gmail.com>
2022-05-02 08:31:55 +0900

    clang_parseTranslationUnit

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,27 @@
 const std = @import("std");
 
-const clang = @cImport({
+const libclang = @cImport({
     @cInclude("clang-c/Index.h");
 });
 
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
 
-    const index = clang.clang_createIndex(0, 0);
-    defer clang.clang_disposeIndex(index);
+    const index = libclang.clang_createIndex(0, 0);
+    defer libclang.clang_disposeIndex(index);
+
+    const source_name = "tmp.cpp";
+    const command_line = [_] [] const u8{};
+    const flags = 0;
+    const unsaved_files = [_]libclang.CXUnsavedFile{};
+    const tu = libclang.clang_parseTranslationUnit(index, source_name, 
+        //command_line, 
+        null,
+        command_line.len, 
+        // unsaved_files, 
+        null,
+        unsaved_files.len, flags);
+    defer libclang.clang_disposeTranslationUnit(tu);
 }
 
 test "basic test" {
